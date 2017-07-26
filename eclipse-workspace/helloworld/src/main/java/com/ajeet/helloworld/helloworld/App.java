64bf71a5ed0488484f85814daf5cd4d9989d98f6
ajeet <ajeet@10.0.0.2>
2017-07-26 18:00:36 -0400

    Done

---
+++ eclipse-workspace/helloworld/src/main/java/com/ajeet/helloworld/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.ajeet.helloworld.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
